6fd2ba99b2a0c3035d4b71059dc904efda3fc130
kacper Arendt <kacper.arendt@inspirelabs.pl>
2021-11-29 22:37:09 +0100

    Feat add fetch planet from api

--- src/components/form/ChoosePlanet.tsx
+++ src/components/form/ChoosePlanet.tsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Button, Form, Label} from "../Components";
 import {IStepProps} from "./Main";
 
+interface IPlanet {
+    id: number,
+    name: string
+}
+
 export const ChoosePlanet = (props: IStepProps) => {
+    const [planets, setPlanets] = useState<Array<IPlanet>>();
+
+    useEffect(() => {
+        const fetchPlanets = async () => {
+            let planetArr: Array<IPlanet> = []
+            const request = await fetch('https://swapi.dev/api/planets/')
+            const response = await request.json();
+
+            if (response.results) {
+                response.results.forEach((planet: IPlanet) => {
+                    planetArr.push({id: planet.id, name: planet.name})
+                })
+                setPlanets(planetArr);
+            }
+        }
+
+        fetchPlanets()
+    }, [])
 
     return (
-        <Form onSubmit={props.nextStep}>
-            <Label>
-                Choose planet:
-                <select name="planet">
-                    <option value="earth">Earth</option>
-                </select>
-            </Label>
-            <Button type='button' onClick={props.prevStep}>Back</Button>
-            <Button type='submit'>Next</Button>
-        </Form>
+            <Form onSubmit={props.nextStep}>
+                <Label>
+                    Choose planet:
+                    <select name="planet">
+                        {planets &&
+                        <>
+                            {planets.map(el => {
+                                return <option value={el.name} key={el.name}>{el.name}</option>
+                            })}
+                        </>
+                        }
+                    </select>
+                </Label>
+                <Button type='button' onClick={props.prevStep}>Back</Button>
+                <Button type='submit'>Next</Button>
+            </Form>
     )
 }
